a2b8210f54cbc5767ad70594023a3245172b47e7
John L. Armstrong IV <jlarmstrongiv@gmail.com>
2020-12-02 14:49:19 -0500

    feat: 🎸 more logs (trigger new minor release)

--- .github/actions/decompress/src/main.ts
+++ .github/actions/decompress/src/main.ts
@@ -1,9 +1,14 @@
 import decompressZips from './decompressZips';
 import deleteZips from './deleteZips';
+import fg from 'fast-glob';
+import path from 'path';
+import * as core from '@actions/core';
 
 export default async function main() {
   await decompressZips();
   await deleteZips();
+  const newPaths = await fg([path.join(process.cwd(), '**/*.br')]);
+  core.info(`newPaths: ${JSON.stringify(newPaths)}`);
 }
 
 if (process.env.NODE_ENV === 'development') {
