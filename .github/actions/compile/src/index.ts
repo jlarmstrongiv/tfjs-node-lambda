36e39a26ace7f755ca7bf2b433a20ef6ebd403e9
John L. Armstrong IV <jlarmstrongiv@gmail.com>
2021-05-12 20:20:53 -0400

    fix: 🐛 naming consistency
    ✅ Closes: #5

--- .github/actions/compile/src/index.ts
+++ .github/actions/compile/src/index.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core';
 import main from './main';
 
 try {
+  // keep format for backwards compatibility nodejs10.x-tf1.7.4.br
   const lambda = `nodejs${core.getInput('lambda')}.x`;
   const tensorflow = core.getInput('tensorflow');
   const binary = core.getInput('binary');
